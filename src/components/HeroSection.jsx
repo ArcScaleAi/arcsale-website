28550b7f6070c42e403828845b30d9048516e491
Gyanendra Verma <gyanendrav416@gmail.com>
2024-06-01 09:57:07 +0530

    Gyanendra/firebase integration (#11)
    * made small changes in ui
    
    * added database content to code base
    
    * Delete .env
    
    ---------
    
    Co-authored-by: ArcScaleAi <167444616+ArcScaleAi@users.noreply.github.com>

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
-import { DemoButton } from '../utils'
-import Navbar from './Navbar'
+import React, { useEffect, useState } from 'react'
+import { DemoButton, HighlightedTitle, HighlightedWord } from '../utils'
+import { onValue, ref } from 'firebase/database'
+import { database } from '../utils/firebaseConfig'
 
 const HeroSection = () => {
+  const [heroContent, setHeroContent] = useState('')
+
+  useEffect(() => {
+    onValue(ref(database, 'data/main'), (snapshot) => {
+      if(snapshot !== null){
+        setHeroContent(snapshot.val())
+      }
+    })
+  }, [])
+
+
   return (
     <section className='w-full h-full relative'>
         <img className='absolute top-0 left-0 h-full w-full -z-10' src="./bg-arcsale.png" alt="bg" />
@@ -10,13 +22,13 @@ const HeroSection = () => {
         <header className='w-full md:py-20 py-8'>
           <div className='w-11/12 mx-auto flex md:flex-row flex-col justify-between gap-10'>
             <div className='mt-20 basis-3/5'>
-              <p className='md:text-[42px] text-3xl text-center md:text-start leading-8 font-bold md:leading-[52px]'><span className='text-color-primary text-center md:text-start'>Virtual Assistant</span> For Superior In-Person Customer Experiences And World-Class Software</p>
-              <p className='py-10 text-xl leading-8 text-center md:text-start'>Our Virtual Assistants Collaborate With Your Human Workforce On Our Versatile, All-In-One Platform, Delivering A <span className='text-black font-bold'>Premier Customer Experience</span> Starting With Retail Stores.</p>
+              <p className='md:text-[42px] text-3xl text-center md:text-start leading-8 font-bold md:leading-[52px]'>{<HighlightedTitle text={heroContent.title?.split('|')[0]}/>} {heroContent.title?.split('|')[1]}</p>
+              <p className='py-10 text-xl leading-8 text-center md:text-start'>{heroContent.para?.split('|')[0]} {<HighlightedWord text={heroContent.para?.split('|')[1]} />} {heroContent.para?.split('|')[1]}</p>
               <DemoButton />
             </div>
 
             <div className='basis-3/5'>
-              <img src="./girl.png" alt="" />
+              <img src={heroContent.image} alt="" />
             </div>
           </div>
         </header>
--- src/components/Solution.jsx
+++ src/components/Solution.jsx
@@ -1,29 +1,46 @@
-import React from 'react'
-import { DemoButton } from '../utils'
+import React, { useEffect, useState } from 'react'
+import { DemoButton, HighlightedTitle, HighlightedWord } from '../utils'
+import { onValue, ref } from 'firebase/database'
+import { database } from '../utils/firebaseConfig'
 
 const Solution = () => {
+  const [solutionContent, setSolutionContent] = useState('')
+  const [GMTcontent, setGMTcontent] = useState('')
+
+  useEffect(() => {
+    onValue(ref(database, 'data/solution'), (snapshot) => {
+      if(snapshot !== null){
+        setSolutionContent(snapshot.val())
+      }
+    })
+  }, [])
+
+  useEffect(() => {
+    onValue(ref(database, 'data/GMTPlatform'), (snapshot) => {
+      if(snapshot !== null){
+        setGMTcontent(snapshot.val())
+      }
+    })
+  }, [])
+
+  const solutionBox = (image, text1, text2, text3, text4) => {
+    return <div className='mt-5 basis-1/3 flex flex-col gap-4'>
+    <img src={image} alt="" />
+    <p className='text-[28px] font-bold text-center sm:text-start text-color-primary leading-9'>{text1}</p>
+    <p className='text-[22px] leading-7 text-center sm:text-start md:w-[86%] text-medium'>{text2} {text3} {text4}</p>
+  </div>
+  }
+
   return (
     <>
       <section className='w-full pt-20 pb-24'>
         <div className='w-11/12 mx-auto'>
-          <h1 className='w-full sm:text-[40px] sm:leading-[48px] text-2xl font-semibold text-center'><span className='text-color-primary'>Virtual Assistant</span> Offers 3-Way Comprehensive Solution</h1>
+          <h1 className='w-full sm:text-[40px] sm:leading-[48px] text-2xl font-semibold text-center'><HighlightedTitle text={solutionContent.title?.split("|")[0]} /> {solutionContent.title?.split("|")[1]}</h1>
 
           <div className='w-full flex md:flex-row flex-col md:gap-14'>
-            <div className='mt-5 basis-1/3 flex flex-col gap-4'>
-              <img src="./solution-1.png" alt="" />
-              <p className='text-[28px] font-bold text-center sm:text-start text-color-primary leading-9'>Self-Service Customer Empowerment</p>
-              <p className='text-[22px] leading-7 text-center sm:text-start md:w-[86%] text-medium'>Empower customers with real-time, <span className='font-semibold text-black'>personalized</span> services through our feature-rich Virtual Assistant.</p>
-            </div>
-            <div className='mt-5 basis-1/3 flex flex-col gap-4'>
-              <img src="./solution-2.png" alt="" />
-              <p className='text-[28px] font-bold text-center sm:text-start text-color-primary leading-9'>Employee Co-Pilot Assistance</p>
-              <p className='text-[22px] leading-7 text-center sm:text-start md:w-[86%] text-medium'>Our platform automates repetitive tasks and serves as a co-pilot, enhancing <span className='font-semibold text-black'>customer experiences</span> to accelerate sales.</p>
-            </div>
-            <div className='mt-5 basis-1/3 flex flex-col gap-4'>
-              <img src="./solution-3.png" alt="" />
-              <p className='text-[28px] font-bold text-center sm:text-start text-color-primary leading-9'>End-To-End Automation For Businesses</p>
-              <p className='text-[22px] leading-7 text-center sm:text-start md:w-[86%] text-medium'>Our platform offers consolidated backend automation, unlocking <span className='font-semibold text-black'>crucial insights</span> often overlooked with fragmented SAAS solutions.</p>
-            </div>
+            {solutionBox(solutionContent.image1, solutionContent.solHead1, solutionContent.solPara1?.split('|')[0], <HighlightedWord text={solutionContent.solPara1?.split('|')[1]} />, solutionContent.solPara1?.split('|')[2] )}
+            {solutionBox(solutionContent.image2, solutionContent.solHead2, solutionContent.solPara2?.split('|')[0], <HighlightedWord text={solutionContent.solPara2?.split('|')[1]} />, solutionContent.solPara2?.split('|')[2] )}
+            {solutionBox(solutionContent.image3, solutionContent.solHead3, solutionContent.solPara3?.split('|')[0], <HighlightedWord text={solutionContent.solPara3?.split('|')[1]} />, solutionContent.solPara3?.split('|')[2] )}
           </div>
         </div>
       </section>
@@ -33,14 +50,15 @@ const Solution = () => {
 
         <div className='flex md:flex-row flex-col w-11/12 sm:w-5/6 mx-auto gap-6'>
           <div className='mt-20 sm:w-11/12'>
-            <p className='sm:text-[40px] text-3xl text-black leading-8 text-center sm:text-start sm:leading-[55px] font-semibold'>We’re Developing A <span className='text-color-primary'>Unified GTM Platform </span> With An Integrated Virtual Assistant</p>
-            <p className='text-lg text-center md:text-start py-6'>Our Virtual Assistant Engages Directly With <span className='text-black font-semibold'>Customers</span>, Learning Their Preferences, Behaviors, Feedback, And <span className='text-black font-semibold'>Purchasing Patterns</span>.</p>
-            <p className='text-lg pb-7 text-center md:text-start'>Delivering <span className='text-black font-semibold'>Actionable Insights</span> For Targeted Marketing, Dynamic Pricing, Revenue Optimization, Geo-Targeting, And Loyalty Programs, Enhancing <span className='text-black font-semibold'>Customer Engagement</span> And Business Strategy.</p>
+            <p className='sm:text-[40px] text-3xl text-black leading-8 text-center md::text-start sm:leading-[55px] font-semibold'>{GMTcontent.title?.split('|')[0]} {<HighlightedTitle text={GMTcontent.title?.split('|')[1]} /> }
+            {GMTcontent.title?.split('|')[2]}</p>
+            <p className='text-lg text-center md:text-start py-6'>{GMTcontent.para1?.split('|')[0]} <HighlightedWord text={GMTcontent.para1?.split('|')[1]} /> {GMTcontent.para1?.split('|')[2]} <HighlightedWord text={GMTcontent.para1?.split('|')[3]} /> {GMTcontent.para1?.split('|')[4]}</p>
+            <p className='text-lg pb-7 text-center md:text-start'>{GMTcontent.para2?.split('|')[0]} <HighlightedWord text={GMTcontent.para2?.split('|')[1]} /> {GMTcontent.para2?.split('|')[2]} <HighlightedWord text={GMTcontent.para2?.split('|')[3]} /> {GMTcontent.para2?.split('|')[4]}</p>
             <DemoButton />
           </div>
 
           <div>
-            <img src="./bubble.png" alt="" />
+            <img src={GMTcontent.image} alt="" />
           </div>
         </div>
 
