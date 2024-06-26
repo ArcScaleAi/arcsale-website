0f14ad4d536ebf8ffa96b122b56d73dacd5e6897
Gyanendra Verma <gyanendrav416@gmail.com>
2024-06-26 19:38:51 +0530

    Gyanendra/analytics and final changes (#17)
    * changes to improve
    
    * changed access_key
    
    * added analytics

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ import DemoContact from '../utils/DemoContact'
 
 const Contact = () => {
     return (
-        <section className='md:py-10 w-full items-center'>
+        <section className='md:py-10 pb-10 w-full items-center'>
             <h1 className='w-full text-center text-4xl font-bold py-6'>Start Building Your <HighlightedTitle text={"AI-Powered"} /> Future</h1>
             <DemoContact />
         </section>
--- src/components/Genie.jsx
+++ src/components/Genie.jsx
@@ -20,10 +20,10 @@ const Genie = () => {
     <section className='w-full pt-10 pb-28'>
       <div className='w-11/12 mx-auto'>
         <h1 className='w-full text-center text-4xl font-bold'>We’re Your <span className='text-color-primary'>Retail Genie</span></h1>
-        <div className='sm:grid mt-8 grid-cols-3 gap-8'>
+        <div className='md:grid mt-8 grid-cols-3 gap-8'>
           {retialPage.genieBox?.map(genie => (
-            <div key={genie.head} className='bg-[rgba(134,72,239,0.11)] pb-5 mt-16 md:mt-12 rounded-3xl border border-color-primary shadow-2xl p-4 relative'>
-              <img className='h-28 -top-1/2 translate-y-3/4 left-1/2 -translate-x-1/2 absolute' src={`${genie.image}`} alt="" />
+            <div key={genie.head} className='bg-[rgba(134,72,239,0.11)] pb-5 mt-20 md:mt-12 rounded-3xl border border-color-primary shadow-2xl p-4 relative'>
+              <img className='h-28 -top-1/4 left-1/2 -translate-x-1/2 absolute' src={`${genie.image}`} alt="" />
               <p className='text-xl text-color-primary md:mt-16 mt-10 text-center font-semibold py-3'>{genie.head}</p>
               <p className='text-center leading-7 mx-auto text-gray-800 text-[17px] w-[90%]'>{genie.para}</p>
             </div>
